a3575e9e7436230a282dbb091322c092bbc1e777
泉 周 <1146673207@qq.com>
2020-01-14 10:22:09 +0800

    initial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            Console.WriteLine("Please input a line:");
+            s = Console.ReadLine();
+            Console.WriteLine("You have entered:{0}", s);
+        }
+    }
+}
